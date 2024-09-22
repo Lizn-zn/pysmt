6acd93e765a1f6c406e4781ee46b5cb51b1e87b1
Lizn-zn <lizenan1995@foxmail.com>
2024-09-22 10:27:22 +0000

    minors

--- pysmt/formula.py
+++ pysmt/formula.py
@@ -271,6 +271,8 @@ class FormulaManager(object):
             return self.Exp(exponent)
         elif exponent.is_constant(types.REAL, 1/2):
             return self.Sqrt(base)
+        elif exponent.is_constant(types.Real, -1):
+            return self.Div(self.Real(1.0), base)
         elif exponent.is_constant() and exponent.constant_value() < 0.0:
             return self.Div(self.Real(1.0), self.Pow(base, self.Real(-exponent.constant_value())))
         else:
